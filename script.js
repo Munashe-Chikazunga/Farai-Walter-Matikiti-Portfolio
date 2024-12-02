3b6051493061019953c4c5607251a9eda7623dae
Munashe-Chikazunga <munashe.chikazunga@icloud.com>
2024-12-03 04:18:49 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const navToggle = document.getElementById('nav-toggle');
-    const navLinks = document.getElementById('nav-links');
-
-    // Mobile navigation toggle
-    navToggle.addEventListener('click', () => {
-        navLinks.classList.toggle('show-nav');
-    });
-});
